e1a914c6534a1543d1475ccfee275c927ebf54d5
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-11-25 01:10:35 +0800

    1.0.0

---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using HarmonyLib.Tools;
+
+namespace PlateUpManyPlayersMod;
+
+[HarmonyPatch(typeof(Kitchen.PlayerManager), MethodType.Constructor)]
+public static class Patch
+{
+    private static void Postfix(ref int ___MaxPlayers)
+    {
+        ___MaxPlayers = 256;
+    }
+}
+
+[BepInPlugin("lotuspar.plugins.manyplayers", "PlateUpManyPlayersMod", "1.0.0.0")]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "lotuspar.plugins.manyplayers");
+        HarmonyFileLog.Enabled = true;
+    }
+}
