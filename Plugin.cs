060a21a0329b18363b472aa8d7bda0489a234435
lotuspar <91263142+lotuspar@users.noreply.github.com>
2022-11-25 15:22:17 +0800

    Shrink a bit

--- Plugin.cs
+++ Plugin.cs
@@ -6,12 +6,9 @@ using HarmonyLib.Tools;
 namespace PlateUpManyPlayersMod;
 
 [HarmonyPatch(typeof(Kitchen.PlayerManager), MethodType.Constructor)]
-public static class Patch
+public static class MaxPlayersPatch
 {
-    private static void Postfix(ref int ___MaxPlayers)
-    {
-        ___MaxPlayers = 256;
-    }
+    private static void Postfix(ref int ___MaxPlayers) => ___MaxPlayers = 256;
 }
 
 [BepInPlugin("lotuspar.plugins.manyplayers", "PlateUpManyPlayersMod", "1.0.0.0")]
@@ -19,9 +16,6 @@ public class Plugin : BaseUnityPlugin
 {
     private void Awake()
     {
-        // Plugin startup logic
-        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
-
         Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), "lotuspar.plugins.manyplayers");
         HarmonyFileLog.Enabled = true;
     }
